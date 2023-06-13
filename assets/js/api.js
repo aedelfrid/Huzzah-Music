3397c47b70bbc810e5507f885f5bf51e98be8752
BenHwaet <benmdesroches@gmail.com>
2023-06-12 22:00:18 -0300

    Changed search terms to fit new project focus - discography instead of bibliography. Some changes in api.js as we started playing around with fetch requests after acquiring authorization keys and tokens. New APIs are Spotify API and Discogs API as suggested by Alex.

---
+++ assets/js/api.js
@@ -0,0 +1,39 @@
+
+HuzzahMusic-GoodsCentral/0.1 + 'https://aedelfrid.github.io/GoodsCentral/'
+
+var searchTrack = document.querySelector("#searchTrack").value;
+var searchArtist = document.querySelector("#searchArtist").value;
+var searchAlbum = document.querySelector("#searchAlbum").value;
+var searchGenre = document.querySelector("#searchGenre").value;
+
+
+var trackSearch = 'https://api.spotify.com/v1/search?q=' + searchTrack + 'type=track';
+var artistSearch = 'https://api.spotify.com/v1/search?q=' + searchArtist + 'type=artist';
+var albumSearch = 'https://api.spotify.com/v1/search?q=' + searchAlbum + 'type=album';
+var genreSearch = 'https://api.spotify.com/v1/search?q=' + searchGenre + 'type=genre';
+
+console.log(trackSearch)
+console.log(artistSearch);
+console.log(albumSearch)
+console.log(genreSearch)
+
+function generalSearch() {
+
+}
+
+
+function advancedSearch() {
+
+  // if text
+    console.log(advancedSearch)
+
+fetch(search)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+      });
+    
+};
+
