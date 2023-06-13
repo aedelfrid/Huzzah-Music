29679f0c0216b0ed8f3b5bc8b6e92fb42815580f
BenHwaet <benmdesroches@gmail.com>
2023-06-12 23:10:25 -0300

    trying out some styling through css and in html. Initial if... statements to target specific search criteria - track, artist, album, genre

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,39 +1,54 @@
 
-HuzzahMusic-GoodsCentral/0.1 + 'https://aedelfrid.github.io/GoodsCentral/'
+// HuzzahMusic-GoodsCentral/0.1 + 'https://aedelfrid.github.io/GoodsCentral/'
+
+
+
+
+function sendSearch () {
+  var generalSearch = document.querySelector("#formSearch").value;
+
+  console.log(generalSearch);
+  
+  if (generalSearch > 0) {
+
+  }
+}
+
+
+function advancedSearch() {
 
 var searchTrack = document.querySelector("#searchTrack").value;
 var searchArtist = document.querySelector("#searchArtist").value;
 var searchAlbum = document.querySelector("#searchAlbum").value;
 var searchGenre = document.querySelector("#searchGenre").value;
 
+console.log(searchAlbum)
+console.log(searchArtist)
+console.log(searchGenre)
+console.log(searchTrack)
 
-var trackSearch = 'https://api.spotify.com/v1/search?q=' + searchTrack + 'type=track';
-var artistSearch = 'https://api.spotify.com/v1/search?q=' + searchArtist + 'type=artist';
-var albumSearch = 'https://api.spotify.com/v1/search?q=' + searchAlbum + 'type=album';
-var genreSearch = 'https://api.spotify.com/v1/search?q=' + searchGenre + 'type=genre';
-
-console.log(trackSearch)
-console.log(artistSearch);
-console.log(albumSearch)
-console.log(genreSearch)
-
-function generalSearch() {
-
+if (searchTrack > 0) {
+  var trackSearch = 'https://api.spotify.com/v1/search?q=' + searchTrack + 'type=track';
+  console.log(trackSearch)
+} else if (searchArtist > 0) {
+  var artistSearch = 'https://api.spotify.com/v1/search?q=' + searchArtist + 'type=artist';
+  console.log(artistSearch);
+} else if (searchAlbum > 0) {
+  var albumSearch = 'https://api.spotify.com/v1/search?q=' + searchAlbum + 'type=album';
+  console.log(albumSearch)
+} else if (searchGenre > 0) {
+  var genreSearch = 'https://api.spotify.com/v1/search?q=' + searchGenre + 'type=genre';
+  console.log(genreSearch);
 }
-
-
-function advancedSearch() {
-
-  // if text
-    console.log(advancedSearch)
-
-fetch(search)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-        console.log(data);
-      });
-    
 };
+
+// fetch(advancedSearch)
+//     .then(function (response) {
+//       return response.json();
+//     })
+//     .then(function (data) {
+//         console.log(data);
+//       });
+    
+
 
