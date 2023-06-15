f88b1e207fda31f7aaf5ae3b4eabde2583031706
aedelfrid <almck51@gmail.com>
2023-06-15 20:52:28 -0300

    auth will now run on page load and every hour thereafter

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -3,6 +3,16 @@ var accessToken;
 import {spotifyClientID} from './key.js'
 import {spotifySecret} from './key.js'
 
+var searchResults;
+
+var pageNum = 0;
+var searchQuery = 'Electrelane';
+//var types = 'track' + '%2c' + 'artist' ;
+var resultLimit = '50';
+var offset = resultLimit*pageNum;
+
+
+
 async function authorization(data = {}) {
     const result = await fetch('https://accounts.spotify.com/api/token',{
         method:'POST',
@@ -19,16 +29,22 @@ async function authorization(data = {}) {
     categoryGet(accessToken);
 };
 
-
-async function categoryGet(data = {}) {
-    const result = await fetch('https://api.spotify.com/v1/browse/categories?country=CA&limit=50',{
+async function categoryGet(accessToken, data = {}) {
+    const result = await fetch(`https://api.spotify.com/v1/search?q=${searchQuery}&type=track&limit=${resultLimit}&offset=${offset}`,{
         method:'GET',
         headers:{ 'Authorization' : `Bearer ${accessToken}`
     }});
     
     var data = await result.json()
-    console.log(data)
+    
+    searchResults = data.tracks.items
+    console.log(searchResults)
 
+    for (var i=0;i<searchResults.length;i++) {
+        console.log(searchResults[i].name)
+    }
 };
 
-authorization();
+(function callEveryHour() {
+    setInterval(authorization(), 1000 * 60 * 60);
+}())
